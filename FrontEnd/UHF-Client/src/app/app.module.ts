3884d308ac060c3d2c6417035f7e217218c207a8
Calvin Zheng <calvinzheng240@gmail.com>
2018-03-12 17:54:39 -0400

    Main page, Login page, routing, service

---
+++ FrontEnd/UHF-Client/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthorizationService } from './services/authorization.service';
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [
+    AuthorizationService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FrontEnd/UHF-Client/src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username: string;
+  password: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login(form: NgForm) {
+    this.username = form.value['username'];
+    this.password = form.value['password'];
+  }
+
+}
