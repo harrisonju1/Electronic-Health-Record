b984c00c418c24d778bda591a4b3297a16a4013e
flynntan <flynnigan.tan@gmail.com>
2018-03-17 15:51:55 -0400

    updated appt record form

--- FrontEnd/UHF-Client/src/app/app.module.ts
+++ FrontEnd/UHF-Client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 // import { HttpModule } from '@angular/http';
 
