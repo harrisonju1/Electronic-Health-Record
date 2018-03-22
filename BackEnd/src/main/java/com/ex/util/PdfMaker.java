d6bfb029f0e107fc6634756e86f145152ecd04ed
ianb96 <ianb96@gmail.com>
2018-03-22 14:13:07 -0400

    Fix pdf output
    returns a byte[] instead of a string back to the browser.
    adds example link

--- BackEnd/src/main/java/com/ex/util/PdfMaker.java
+++ BackEnd/src/main/java/com/ex/util/PdfMaker.java
@@ -1,12 +1,11 @@
 package com.ex.util;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.commons.lang.CharSet;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 public class PdfMaker {
@@ -23,20 +22,18 @@ public class PdfMaker {
         try {
             output = new ByteArrayOutputStream();
             pdfwriter = PdfWriter.getInstance(doc, output);
-            doc.open();
             doc.addAuthor("Universal Healthcare Force");
             doc.addCreator("Team Triple G Force");
+            doc.open();
         } catch (DocumentException e) {
             e.printStackTrace();
         }
     }
-    public String finishDocument() {
-        doc.close();
-        pdfwriter.close();
+    public Object finishDocument() {
         try {
-            String pdfdata = output.toString("UTF-8");
-            return pdfdata;
-        } catch (UnsupportedEncodingException e) {
+            doc.close();
+            return output.toByteArray();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -54,6 +51,7 @@ public class PdfMaker {
             doc.add(e);
         } catch (DocumentException e1) {
             e1.printStackTrace();
+            return null;
         }
         return this;
     }
--- BackEnd/src/test/java/com/ex/util/PdfMakerTest.java
+++ BackEnd/src/test/java/com/ex/util/PdfMakerTest.java
@@ -1,8 +1,16 @@
 package com.ex.util;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.junit.Assert;
 import org.junit.Test;
+
+import javax.print.Doc;
+
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -14,11 +22,8 @@ public class PdfMakerTest {
         maker.setTitle("PdfTitleTest");
         maker.setSubject("PDF Subject Test");
         maker.add(new Paragraph("Test Text here"));
-        String pdfdata = maker.finishDocument();
+        Object pdfdata = maker.finishDocument();
 //        System.out.println(pdfdata);
-        Assert.assertTrue(pdfdata.contains("Title"));
-        Assert.assertTrue(pdfdata.contains("Subject"));
-        Assert.assertTrue(pdfdata.contains("Text"));
+        Assert.assertNotNull(pdfdata);
     }
-
 }
