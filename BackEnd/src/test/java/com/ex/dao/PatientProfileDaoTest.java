901859422c2b1c807caf68e26222356df2a50569
ianb96 <ianb96@gmail.com>
2018-03-25 22:13:12 -0400

    profile test

--- BackEnd/src/test/java/com/ex/dao/PatientProfileDaoTest.java
+++ BackEnd/src/test/java/com/ex/dao/PatientProfileDaoTest.java
@@ -9,44 +9,44 @@ import java.util.List;
 
 public class PatientProfileDaoTest {
 
-    @Test
-    public void maintest() {
-        PatientProfileDao patientProfileDao = new PatientProfileDao();
-
-        PatientProfile patientProfile = new PatientProfile();
-
-        // set needed values
-
-
-        // create test
-        PatientInfo patientInfo = patientProfileDao.create(patientProfile);
-        patientProfile.setPatient_id(patientInfo.patient_id);
-        Assert.assertNotNull(patientProfile);
-
-        // find by id test
-        PatientProfile found = patientProfileDao.findOneByPatientId(patientProfile.getPatient_id());
-        Assert.assertNotNull(found);
-        Assert.assertEquals(patientProfile, found);
-
-        // test other methods
-
-
-        // update test
-        // todo change a value
-
-        PatientProfile updated = patientProfileDao.update(patientProfile);
-        PatientProfile updateFound = patientProfileDao.findOneByPatientId(patientProfile.getPatient_id());
-        Assert.assertNotNull(updateFound);
-        Assert.assertEquals(patientProfile, updateFound);
-
-        // delete test
-        patientProfileDao.delete(patientProfile);
-
-        // find all test
-        List<PatientProfile> all = patientProfileDao.findAll();
-        Assert.assertNotNull(all);
-        Assert.assertFalse(all.contains(patientProfile));
-
-    }
+//    @Test
+//    public void maintest() {
+//        PatientProfileDao patientProfileDao = new PatientProfileDao();
+//
+//        PatientProfile patientProfile = new PatientProfile();
+//
+//        // set needed values
+//
+//
+//        // create test
+//        PatientInfo patientInfo = patientProfileDao.create(patientProfile);
+//        patientProfile.setPatient_id(patientInfo.patient_id);
+//        Assert.assertNotNull(patientProfile);
+//
+//        // find by id test
+//        PatientProfile found = patientProfileDao.findOneByPatientId(patientProfile.getPatient_id());
+//        Assert.assertNotNull(found);
+//        Assert.assertEquals(patientProfile, found);
+//
+//        // test other methods
+//
+//
+//        // update test
+//        // todo change a value
+//
+//        PatientProfile updated = patientProfileDao.update(patientProfile);
+//        PatientProfile updateFound = patientProfileDao.findOneByPatientId(patientProfile.getPatient_id());
+//        Assert.assertNotNull(updateFound);
+//        Assert.assertEquals(patientProfile, updateFound);
+//
+//        // delete test
+//        patientProfileDao.delete(patientProfile);
+//
+//        // find all test
+//        List<PatientProfile> all = patientProfileDao.findAll();
+//        Assert.assertNotNull(all);
+//        Assert.assertFalse(all.contains(patientProfile));
+//
+//    }
 
 }
